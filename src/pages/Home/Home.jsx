f8398e282c5a44cae9e54f91060bf077a5bff0d5
robiul-romjan <robiulromjan7@gmail.com>
2023-08-11 00:43:06 +0600

    add somr tag

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1>Hello World</h1>
+            <h1>New Hello </h1>
+            <h1>New World </h1>
+            <h1>New Hello </h1>
         </div>
     );
 };
