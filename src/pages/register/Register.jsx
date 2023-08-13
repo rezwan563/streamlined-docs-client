a6f5ff502ad32f51d6233d906c7a8157bd5cdd25
robiul-romjan <robiulromjan7@gmail.com>
2023-08-13 14:51:21 +0600

    design login page

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,8 +1,94 @@
+import { FcGoogle } from 'react-icons/fc'
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
-        <div>
-            Register
+        <div className='flex justify-center items-center min-h-screen py-12'>
+            <div className='flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-gray-100 text-gray-900'>
+                <div className='mb-8 text-center'>
+                    <h1 className='my-3 text-4xl font-bold'>Log In</h1>
+                    <p className='text-sm text-gray-400'>
+                        Sign in to access your account
+                    </p>
+                </div>
+                <form
+                    noValidate=''
+                    action=''
+                    className='space-y-6 ng-untouched ng-pristine ng-valid'
+                >
+                    <div className='grid grid-cols-2 gap-4'>
+                        <div>
+                            <label htmlFor='email' className='block mb-2 text-sm'>
+                                Email address
+                            </label>
+                            <input
+                                type='email'
+                                name='email'
+                                id='email'
+                                required
+                                placeholder='Enter Your Email Here'
+                                className='w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-green-500 bg-gray-200 text-gray-900'
+                                data-temp-mail-org='0'
+                            />
+                        </div>
+                        <div>
+                            <div className='flex justify-between'>
+                                <label htmlFor='password' className='text-sm mb-2'>
+                                    Password
+                                </label>
+                            </div>
+                            <input
+                                type='password'
+                                name='password'
+                                id='password'
+                                required
+                                placeholder='*******'
+                                className='w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-green-500 bg-gray-200 text-gray-900'
+                            />
+                        </div>
+                    </div>
+
+                    <div>
+                        <button
+                            type='submit'
+                            className='bg-green-500 w-full rounded-md py-3 text-white'
+                        >
+                            Log In
+                        </button>
+                    </div>
+                </form>
+                <div className='space-y-1'>
+                    <button
+                        className='text-xs hover:underline hover:text-rose-500 text-gray-400'
+                    >
+                        Forgot password?
+                    </button>
+                </div>
+                <div className='flex items-center pt-4 space-x-1'>
+                    <div className='flex-1 h-px sm:w-16 dark:bg-gray-700'></div>
+                    <p className='px-3 text-sm dark:text-gray-400'>
+                        Login with social accounts
+                    </p>
+                    <div className='flex-1 h-px sm:w-16 dark:bg-gray-700'></div>
+                </div>
+                <div
+                    className='flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer'
+                >
+                    <FcGoogle size={32} />
+
+                    <p>Continue with Google</p>
+                </div>
+                <p className='px-6 text-sm text-center text-gray-400'>
+                    Do not have an account yet?{' '}
+                    <Link
+                        to='/signUp'
+                        className='hover:underline hover:text-green-500 text-gray-600'
+                    >
+                        Sign up
+                    </Link>
+                    .
+                </p>
+            </div>
         </div>
     );
 };
