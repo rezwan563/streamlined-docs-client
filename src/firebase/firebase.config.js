6fd9a22913a26b7b04d8b8f55d8bc9f0695714d5
robiul-romjan <robiulromjan7@gmail.com>
2023-08-15 20:05:10 +0600

    Committing my changes before merge

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,15 +1,33 @@
+// import { initializeApp } from "firebase/app";
+
+
+// const firebaseConfig = {
+// apiKey: "AIzaSyBiyRJBxAQVhJjvYc-sCgH77bXESBhxRn8",
+// authDomain: "streamlined-docs.firebaseapp.com",
+// projectId: "streamlined-docs",
+// storageBucket: "streamlined-docs.appspot.com",
+// messagingSenderId: "258103047374",
+// appId: "1:258103047374:web:9ad44fc0ab126112288c9f"
+// };
+
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-
+// Your web app's Firebase configuration
 const firebaseConfig = {
-apiKey: "AIzaSyBiyRJBxAQVhJjvYc-sCgH77bXESBhxRn8",
-authDomain: "streamlined-docs.firebaseapp.com",
-projectId: "streamlined-docs",
-storageBucket: "streamlined-docs.appspot.com",
-messagingSenderId: "258103047374",
-appId: "1:258103047374:web:9ad44fc0ab126112288c9f"
+  apiKey: "AIzaSyAi711E7KbbyZnJjhD6lnEHTx7yogDTicE",
+  authDomain: "question-bank-47506.firebaseapp.com",
+  projectId: "question-bank-47506",
+  storageBucket: "question-bank-47506.appspot.com",
+  messagingSenderId: "197952308307",
+  appId: "1:197952308307:web:cca857afa3969030f772a6"
 };
-
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
