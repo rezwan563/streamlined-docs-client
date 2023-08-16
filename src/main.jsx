567f0234f589846d326e320e60c2266bcb60e199
robiul-romjan <robiulromjan7@gmail.com>
2023-08-16 20:21:00 +0600

    add react stricmode

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
+/* eslint-disable react/jsx-no-undef */
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { RouterProvider} from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
 import router from './routes/router'
 import AuthProvider from './providers/AuthProvider'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <AuthProvider>
-    <RouterProvider router={router}/>
-  </AuthProvider>,
+  <React.StrictMode>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
+  </React.StrictMode>,
 )
