2345cee52cd6c3decf09db164f9ccec287c4995f
robiul-romjan <robiulromjan7@gmail.com>
2023-08-16 20:33:19 +0600

    add react dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
-/* eslint-disable react/jsx-no-undef */
+
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/router'
 import AuthProvider from './providers/AuthProvider'
+import React from 'react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
