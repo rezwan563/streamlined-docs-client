74a80e5e5ecdf659a7b6a139cc502707f8a19d73
raaz1212 <araj30728@gmail.com>
2023-08-17 22:55:00 +0600

    useeffect for cookies jwt

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import { createContext, useEffect, useState } from 'react'
+import { createContext, useEffect, useState } from "react";
 import {
   GoogleAuthProvider,
   createUserWithEmailAndPassword,
@@ -10,59 +10,74 @@ import {
   signInWithPopup,
   signOut,
   updateProfile,
-} from 'firebase/auth'
-import { app } from '../firebase/firebase.config'
+} from "firebase/auth";
+import { app } from "../firebase/firebase.config";
+import axios from "axios";
+import Cookies from "js-cookie";
 
-export const AuthContext = createContext("")
-const auth = getAuth(app)
-const googleProvider = new GoogleAuthProvider()
+export const AuthContext = createContext("");
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 
 const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null)
-  const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   const createUser = (email, password) => {
-    setLoading(true)
-    return createUserWithEmailAndPassword(auth, email, password)
-  }
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
 
   const signIn = (email, password) => {
-    setLoading(true)
-    return signInWithEmailAndPassword(auth, email, password)
-  }
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
 
   const signInWithGoogle = () => {
-    setLoading(true)
-    return signInWithPopup(auth, googleProvider)
-  }
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
 
-  const resetPassword = email => {
-    setLoading(true)
-    return sendPasswordResetEmail(auth, email)
-  }
+  const resetPassword = (email) => {
+    setLoading(true);
+    return sendPasswordResetEmail(auth, email);
+  };
 
   const logOut = () => {
-    setLoading(true)
-    return signOut(auth)
-  }
+    setLoading(true);
+    return signOut(auth);
+  };
 
   const updateUserProfile = (fullName, phone) => {
     return updateProfile(auth.currentUser, {
       displayName: fullName,
-      phoneNumber: phone
-    })
-  }
+      phoneNumber: phone,
+    });
+  };
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, currentUser => {
-      setUser(currentUser)
-    //   console.log('current user', currentUser)
-      setLoading(false)
-    })
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      // get and set token
+      if (currentUser) {
+        axios
+          .post(`${import.meta.env.VITE_SERVER_API}/jwt`, {
+            email: currentUser.email,
+          })
+          .then((data) => {
+            // Use js-cookie to set the token as a cookie
+            Cookies.set("access-token", data.data.token);
+            setLoading(false);
+          });
+      } else {
+        // Remove the token cookie
+        Cookies.remove("access-token");
+      }
+    });
     return () => {
-      return unsubscribe()
-    }
-  }, [])
+      return unsubscribe;
+    };
+  }, []);
 
   const authInfo = {
     user,
@@ -74,11 +89,11 @@ const AuthProvider = ({ children }) => {
     resetPassword,
     logOut,
     updateUserProfile,
-  }
+  };
 
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
-  )
-}
+  );
+};
 
-export default AuthProvider
+export default AuthProvider;
