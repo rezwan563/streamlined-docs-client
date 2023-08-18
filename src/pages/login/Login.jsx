75966919d3cd960819aa036b6f3f3bdd239a36e7
Rezwan <t.rewan17@gmail.com>
2023-08-18 19:14:29 +0600

    Copied login from robiul branch

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,121 +1,49 @@
-
 import { useContext, useState } from 'react';
 import { FcGoogle } from 'react-icons/fc'
-import { Link,  useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../providers/AuthProvider';
 import Swal from 'sweetalert2'
-import axios from 'axios';
-
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import axios from 'axios'
 const Login = () => {
+    const [show, setShow] = useState(true);
     const [error, setError] = useState("");
     const { signIn, signInWithGoogle } = useContext(AuthContext);
-    const navigate = useNavigate();
-    const from =  '/dashboard' ;
+    const navigate = useNavigate()
+    const from =  '/dashboard';
 
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const form = e.target;
-    const email = form.email.value;
-    const password = form.password.value;
+    const handleSubmit = e => {
+        e.preventDefault();
+        const form = e.target;
+        const email = form.email.value;
+        const password = form.password.value;
 
-    setError("");
+        setError("")
 
-    signIn(email, password)
-      .then((result) => {
-        console.log(result.user);
-        Swal.fire({
-          title: "Success!",
-          text: "You have successfully Login",
-          icon: "success",
-          confirmButtonText: "Ok",
-        });
-        navigate("/");
-      })
-      .catch((error) => {
-        setError(error.message);
-      });
-    form.reset();
-  };
-
-  const handleGoogleSignIn = () => {
-    signInWithGoogle()
-      .then((result) => {
-        console.log(result.user);
-        navigate(from, { replace: true });
-      })
-      .catch((error) => {
-        setError(error.message);
-      });
-  };
-
-
-  return (
-    <div className="flex justify-center items-center min-h-screen bg-green-100 sm:py-12">
-      <div className="flex flex-col max-w-md p-6 rounded-md sm:p-5 w-full bg-gray-100 text-gray-900">
-        <div className="mb-8 text-center">
-          <h1 className="my-3 text-4xl font-bold">Log In</h1>
-          <p className="text-sm text-gray-400">
-            Sign in to access your account
-          </p>
-        </div>
-        <form
-          onSubmit={handleSubmit}
-          noValidate=""
-          action=""
-          className="space-y-6 ng-untouched ng-pristine ng-valid"
-        >
-          <div className="space-y-4">
-            <div>
-              <label htmlFor="email" className="block mb-2 text-sm">
-                Email address
-              </label>
-              <input
-                type="email"
-                name="email"
-                id="email"
-                required
-                placeholder="Enter Your Email Here"
-                className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-green-500 bg-gray-200 text-gray-900"
-                data-temp-mail-org="0"
-              />
-            </div>
-            <div>
-              <div className="flex justify-between">
-                <label htmlFor="password" className="text-sm mb-2">
-                  Password
-                </label>
-              </div>
-              <input
-                type="password"
-                name="password"
-                id="password"
-                required
-                placeholder="*******"
-                className="w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-green-500 bg-gray-200 text-gray-900"
-              />
+        signIn(email, password)
+            .then(result => {
+                console.log(result.user)
+                Swal.fire({
+                    title: 'Success!',
+                    text: 'You have successfully Login',
+                    icon: 'success',
+                    confirmButtonText: 'Ok'
+                })
+                navigate("/")
+            })
+            .catch(error => {
+                setError(error.message)
+            })
+        form.reset()
+    }
 
     const handleGoogleSignIn = () => {
         signInWithGoogle()
-        .then((r) => {
-            const loggedUser = r.user;
-            console.log(loggedUser);
-            const savedUser = { name: loggedUser.displayName, email: loggedUser.email };
-            
-            // Use axios to make the POST request to the local server
-            axios.post('http://localhost:5000/users', savedUser, {
-                headers: {
-                    'Content-Type': 'application/json'
-                }
+            .then(result => {
+                console.log(result.user)
+                navigate(from, { replace: true })
             })
-            .then(() => {
-                
-                navigate(from);
-            })
-            .catch(error => {
-                console.error(error);
-            });
-        })
             .catch(error => {
                 setError(error.message)
             })
@@ -151,20 +79,23 @@ const Login = () => {
                                 data-temp-mail-org='0'
                             />
                         </div>
-                        <div>
+                        <div className='relative'>
                             <div className='flex justify-between'>
                                 <label htmlFor='password' className='text-sm mb-2'>
                                     Password
                                 </label>
                             </div>
                             <input
-                                type='password'
+                                type={show ? "password" : "text"}
                                 name='password'
                                 id='password'
                                 required
                                 placeholder='*******'
                                 className='w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-green-500 bg-gray-200 text-gray-900'
                             />
+                            <span onClick={() => setShow(!show)} className="cursor-pointer absolute right-0 top-1/2 mt-2 me-4">
+                                {show ? <FaEyeSlash /> : <FaEye />}
+                            </span>
                         </div>
                     </div>
                     <p className='text-center text-red-500 font-medium'>{error}</p>
@@ -193,10 +124,11 @@ const Login = () => {
                 </div>
                 <div
                     onClick={handleGoogleSignIn}
-                    className='flex justify-center items-center  border m-3 p-2 border-gray-300 border-rounded cursor-pointer'
+                    className='flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer'
                 >
-                    <div className='flex space-x-2 items-center'><FcGoogle size={32} />
-                    <p>Continue with Google</p></div>
+                    <FcGoogle size={32} />
+
+                    <p>Continue with Google</p>
                 </div>
                 <p className='px-6 text-sm text-center text-gray-400'>
                     Do not have an account?
@@ -208,52 +140,9 @@ const Login = () => {
                     </Link>
                     .
                 </p>
-
             </div>
-          </div>
-          <p className="text-center text-red-500 font-medium">{error}</p>
-          <div>
-            <button
-              type="submit"
-              className="bg-green-500 hover:bg-green-700 transition-all w-full rounded-md py-3 text-white"
-            >
-              Log In
-            </button>
-          </div>
-        </form>
-        <div className="space-y-1">
-          <button className="text-xs hover:underline hover:text-rose-500 text-gray-400">
-            Forgot password?
-          </button>
         </div>
-        <div className="flex items-center pt-4 space-x-1">
-          <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
-          <p className="px-3 text-sm dark:text-gray-400">
-            Login with social accounts
-          </p>
-          <div className="flex-1 h-px sm:w-16 dark:bg-gray-700"></div>
-        </div>
-        <div
-          onClick={handleGoogleSignIn}
-          className="flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer"
-        >
-          <FcGoogle size={32} />
-
-          <p>Continue with Google</p>
-        </div>
-        <p className="px-6 text-sm text-center text-gray-400">
-          Do not have an account?
-          <Link
-            to="/register"
-            className="hover:underline hover:text-green-500 text-gray-600"
-          >
-            Sign up
-          </Link>
-          .
-        </p>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Login;
