a8ca428413ac84a43a51af3f3c8d8d552e9aea30
robiul-romjan <robiulromjan7@gmail.com>
2023-08-21 16:15:42 +0600

    add animation to login and register page

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -4,6 +4,8 @@ import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../providers/AuthProvider';
 import Swal from 'sweetalert2'
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
+import Lottie from "lottie-react";
+import regAnim from "../../assets/animation_llgf060i.json"
 
 const Register = () => {
     const [show, setShow] = useState(true);
@@ -70,143 +72,146 @@ const Register = () => {
     }
 
     return (
-        <div className='flex justify-center items-center min-h-screen sm:py-12 bg-green-100'>
-            <div className='flex flex-col max-w-lg p-6 rounded-md sm:p-5 w-full bg-gray-100 text-gray-900'>
-                <div className='mb-8 text-center'>
-                    <h1 className='my-3 text-4xl font-bold'>Sign Up</h1>
-                    <p className='text-sm text-gray-400'>
-                        Welcome to our passport app
-                    </p>
-                </div>
-                <form
-                    onSubmit={handleSubmit}
-                    noValidate=''
-                    action=''
-                    className='space-y-6 ng-untouched ng-pristine ng-valid'
-                >
-                    <div className='grid md:grid-cols-2 gap-4'>
-                        <div>
-                            <label htmlFor='email' className='block mb-2 text-sm'>
-                                First Name
-                            </label>
-                            <input
-                                type='text'
-                                name='first_name'
-                                required
-                                placeholder='first name'
-                                className='w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-green-500 bg-gray-200 text-gray-900'
-                                data-temp-mail-org='0'
-                            />
-                        </div>
-                        <div>
-                            <label htmlFor='email' className='block mb-2 text-sm'>
-                                Last Name
-                            </label>
-                            <input
-                                type='text'
-                                name='last_name'
-                                required
-                                placeholder='last name'
-                                className='w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-green-500 bg-gray-200 text-gray-900'
-                                data-temp-mail-org='0'
-                            />
-                        </div>
-                        <div>
-                            <label htmlFor='email' className='block mb-2 text-sm'>
-                                Email address
-                            </label>
-                            <input
-                                type='email'
-                                name='email'
-                                required
-                                placeholder='type email address'
-                                className='w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-green-500 bg-gray-200 text-gray-900'
-                                data-temp-mail-org='0'
-                            />
-                        </div>
-                        <div>
-                            <label htmlFor='email' className='block mb-2 text-sm'>
-                                Phone Number
-                            </label>
-                            <input
-                                type='number'
-                                name='phone'
-                                required
-                                placeholder='type phone number'
-                                className='w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-green-500 bg-gray-200 text-gray-900'
-                                data-temp-mail-org='0'
-                            />
-                        </div>
-                        <div className='relative'>
-                            <div className='flex justify-between'>
-                                <label htmlFor='password' className='text-sm mb-2'>
-                                    Password
-                                </label>
-                            </div>
-                            <input
-                                type={show ? "password" : "text"}
-                                name='password'
-                                required
-                                placeholder='*******'
-                                className='w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-green-500 bg-gray-200 text-gray-900'
-                            />
-                            <span onClick={() => setShow(!show)} className="cursor-pointer absolute right-0 top-1/2 mt-2 me-4">
-                                {show ? <FaEyeSlash /> : <FaEye />}
-                            </span>
-                        </div>
-                        <div className='relative'>
-                            <div className='flex justify-between'>
-                                <label htmlFor='password' className='text-sm mb-2'>
-                                    Confirm Password
-                                </label>
-                            </div>
-                            <input
-                                type={showConfirm ? "password" : "text"}
-                                name='confirm_password'
-                                required
-                                placeholder='*******'
-                                className='w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-green-500 bg-gray-200 text-gray-900'
-                            />
-                            <span onClick={() => setShowConfirm(!showConfirm)} className="cursor-pointer absolute right-0 top-1/2 mt-2 me-4">
-                                {showConfirm ? <FaEyeSlash /> : <FaEye />}
-                            </span>
-                        </div>
+        <div className='grid md:grid-cols-2'>
+            <Lottie className='hidden md:block h-[100vh]' animationData={regAnim} />
+            <div className='flex justify-center items-center min-h-screen sm:py-12 bg-green-100'>
+                <div className='flex flex-col max-w-lg p-6 rounded-md sm:p-5 w-full bg-gray-100 text-gray-900'>
+                    <div className='mb-8 text-center'>
+                        <h1 className='my-3 text-4xl font-bold'>Sign Up</h1>
+                        <p className='text-sm text-gray-400'>
+                            Welcome to our passport app
+                        </p>
                     </div>
-                    <p className='text-center text-red-500 font-medium'>{error}</p>
-                    <div>
-                        <button
-                            type='submit'
-                            className='bg-green-500 hover:bg-green-700 transition-all w-full rounded-md py-3 text-white'
-                        >
-                            Sign Up
-                        </button>
-                    </div>
-                </form>
-
-                <div className='flex items-center pt-4 space-x-1'>
-                    <div className='flex-1 h-px sm:w-16 dark:bg-gray-700'></div>
-                    <p className='px-3 text-sm dark:text-gray-400'>
-                        Sign up with social accounts
-                    </p>
-                    <div className='flex-1 h-px sm:w-16 dark:bg-gray-700'></div>
-                </div>
-                <div
-                    onClick={handleGoogleSignIn}
-                    className='flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer'
-                >
-                    <FcGoogle size={32} />
-
-                    <p>Continue with Google</p>
-                </div>
-                <p className='px-6 text-sm text-center text-gray-400'>
-                    Have an account?
-                    <Link
-                        to='/login'
-                        className='hover:underline hover:text-green-500 text-gray-600'
+                    <form
+                        onSubmit={handleSubmit}
+                        noValidate=''
+                        action=''
+                        className='space-y-6 ng-untouched ng-pristine ng-valid'
                     >
-                        Login here
-                    </Link>
-                </p>
+                        <div className='grid md:grid-cols-2 gap-4'>
+                            <div>
+                                <label htmlFor='email' className='block mb-2 text-sm'>
+                                    First Name
+                                </label>
+                                <input
+                                    type='text'
+                                    name='first_name'
+                                    required
+                                    placeholder='first name'
+                                    className='w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-green-500 bg-gray-200 text-gray-900'
+                                    data-temp-mail-org='0'
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor='email' className='block mb-2 text-sm'>
+                                    Last Name
+                                </label>
+                                <input
+                                    type='text'
+                                    name='last_name'
+                                    required
+                                    placeholder='last name'
+                                    className='w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-green-500 bg-gray-200 text-gray-900'
+                                    data-temp-mail-org='0'
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor='email' className='block mb-2 text-sm'>
+                                    Email address
+                                </label>
+                                <input
+                                    type='email'
+                                    name='email'
+                                    required
+                                    placeholder='type email address'
+                                    className='w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-green-500 bg-gray-200 text-gray-900'
+                                    data-temp-mail-org='0'
+                                />
+                            </div>
+                            <div>
+                                <label htmlFor='email' className='block mb-2 text-sm'>
+                                    Phone Number
+                                </label>
+                                <input
+                                    type='number'
+                                    name='phone'
+                                    required
+                                    placeholder='type phone number'
+                                    className='w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-green-500 bg-gray-200 text-gray-900'
+                                    data-temp-mail-org='0'
+                                />
+                            </div>
+                            <div className='relative'>
+                                <div className='flex justify-between'>
+                                    <label htmlFor='password' className='text-sm mb-2'>
+                                        Password
+                                    </label>
+                                </div>
+                                <input
+                                    type={show ? "password" : "text"}
+                                    name='password'
+                                    required
+                                    placeholder='*******'
+                                    className='w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-green-500 bg-gray-200 text-gray-900'
+                                />
+                                <span onClick={() => setShow(!show)} className="cursor-pointer absolute right-0 top-1/2 mt-2 me-4">
+                                    {show ? <FaEyeSlash /> : <FaEye />}
+                                </span>
+                            </div>
+                            <div className='relative'>
+                                <div className='flex justify-between'>
+                                    <label htmlFor='password' className='text-sm mb-2'>
+                                        Confirm Password
+                                    </label>
+                                </div>
+                                <input
+                                    type={showConfirm ? "password" : "text"}
+                                    name='confirm_password'
+                                    required
+                                    placeholder='*******'
+                                    className='w-full px-3 py-2 border rounded-md border-gray-300 focus:outline-green-500 bg-gray-200 text-gray-900'
+                                />
+                                <span onClick={() => setShowConfirm(!showConfirm)} className="cursor-pointer absolute right-0 top-1/2 mt-2 me-4">
+                                    {showConfirm ? <FaEyeSlash /> : <FaEye />}
+                                </span>
+                            </div>
+                        </div>
+                        <p className='text-center text-red-500 font-medium'>{error}</p>
+                        <div>
+                            <button
+                                type='submit'
+                                className='bg-green-500 hover:bg-green-700 transition-all w-full rounded-md py-3 text-white'
+                            >
+                                Sign Up
+                            </button>
+                        </div>
+                    </form>
+
+                    <div className='flex items-center pt-4 space-x-1'>
+                        <div className='flex-1 h-px sm:w-16 dark:bg-gray-700'></div>
+                        <p className='px-3 text-sm dark:text-gray-400'>
+                            Sign up with social accounts
+                        </p>
+                        <div className='flex-1 h-px sm:w-16 dark:bg-gray-700'></div>
+                    </div>
+                    <div
+                        onClick={handleGoogleSignIn}
+                        className='flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer'
+                    >
+                        <FcGoogle size={32} />
+
+                        <p>Continue with Google</p>
+                    </div>
+                    <p className='px-6 text-sm text-center text-gray-400'>
+                        Have an account?
+                        <Link
+                            to='/login'
+                            className='hover:underline hover:text-green-500 text-gray-600'
+                        >
+                            Login here
+                        </Link>
+                    </p>
+                </div>
             </div>
         </div>
     );
