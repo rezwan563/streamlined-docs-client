3837bb6d4953a49a700ec9080dd1236f49aae09b
Rezwan <t.rewan17@gmail.com>
2023-08-27 00:31:09 +0600

    Changes in DashboardLayout

--- src/layout/DashboardLayout.jsx
+++ src/layout/DashboardLayout.jsx
@@ -1,6 +1,5 @@
-
 import { useContext } from "react";
-import {  Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import { AuthContext } from "../providers/AuthProvider";
 import Sidebar from "../shared/Sidebar";
 
@@ -8,7 +7,6 @@ const DashboardLayout = () => {
   const { user } = useContext(AuthContext);
   // console.log(user);
 
-  
   return (
     <div>
       <div className="w-full bg-white py-2">
@@ -22,55 +20,44 @@ const DashboardLayout = () => {
             placeholder="search"
             className="hidden md:block px-2 rounded-md border focus:outline-none focus:ring-2 focus:ring-green-300 focus:border-transparent"
           /> */}
-          {/* <select className="hidden md:block w-fit px-4 py-1 border rounded-md focus:outline-none focus:ring-2 focus:ring-green-300 focus:border-transparent">
-            <option value="Bangla" className="py-1">
-              Bangla
-            </option>
-            <option value="English" className="py-1">
-              English
-            </option>
-          </select> */}
           <img src={user?.photoURL} alt="" className="w-12 h-12 rounded-full" />
         </div>
       </div>
       {/* for small device grid row and outlet at top */}
       <div className="grid grid-rows-3">
         <div className="md:hidden row-span-2 bg-gray-100 px-24 py-12">
-          <Outlet/>
+          <Outlet />
         </div>
         <div className="md:hidden ">
-          <Sidebar/>
+          <Sidebar />
         </div>
       </div>
       {/* for medium device sidebar  sidebar and outlet  */}
       <div className="grid grid-cols-6">
-          <div className="hidden md:block lg:hidden min-h-screen col-span-2">
-            <Sidebar/>
+        <div className="hidden md:block lg:hidden min-h-screen col-span-2">
+          <Sidebar />
+        </div>
+        <div className="hidden md:block lg:hidden bg-gray-100 col-span-4">
+          <div className="py-24 px-12">
+            <Outlet />
           </div>
-          <div className="hidden md:block lg:hidden bg-gray-100 col-span-4">
-            <div className="py-24 px-12">
-            <Outlet/>
-            </div>
-          </div>
+        </div>
       </div>
       {/* for large device desktop */}
       <div className="max-w-[1920px] mx-auto">
-      <div className="grid grid-cols-10">
+        <div className="grid grid-cols-10">
           <div className="hidden  lg:block  min-h-screen col-span-2">
-            <Sidebar/>
+            <Sidebar />
           </div>
           <div className="hidden lg:block bg-gray-100 col-span-8">
             <div className="py-24 px-12">
-            <Outlet/>
+              <Outlet />
             </div>
           </div>
+        </div>
       </div>
-      </div>
-    
     </div>
   );
 };
 
 export default DashboardLayout;
-
-
