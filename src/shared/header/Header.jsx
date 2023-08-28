6850aef5031799859941cb8109b28a2c79b0deea
Rezwan <t.rewan17@gmail.com>
2023-08-29 04:38:34 +0600

    Sliding from navbar for tablet and mobile device

--- src/shared/header/Header.jsx
+++ src/shared/header/Header.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import ActiveLink from "../activelink/ActiveLink";
 import { HiMenuAlt3 } from "react-icons/hi";
 const Header = () => {
@@ -8,10 +9,13 @@ const Header = () => {
       <ActiveLink to="/">FAQ</ActiveLink>
     </>
   );
+  const [isClicked, setIsClicked] = useState(false);
+
+ 
   return (
-    <nav className="">
+    <nav className="max-w-[1980px]  lg:mx-auto">
       {/* desktop */}
-      <div className="flex">
+      <div className="flex justify-around">
         <div className="hidden lg:flex lg:gap-16">
           <div>
             <p>STREAMLINED DOCS</p>
@@ -23,13 +27,20 @@ const Header = () => {
         </div>
       </div>
       {/* mobile device */}
-      <div className="px-12 md:px-16 lg:hidden">
+      <div className="px-10 md:px-16 lg:hidden">
         <div className="flex justify-between">
           <p>STREAMLINED DOCS</p>
-          <div className="hidden">{navLinks}</div>
-        <div><HiMenuAlt3/></div>
+          <div className="hidden lg:block">{navLinks}</div>
+        <div><HiMenuAlt3 onClick={() => setIsClicked(!isClicked)} className="text-4xl"/></div>
         </div>
       </div>
+      {/* transform with transition create sliding effect  */}
+      <div className={`lg:hidden z-50 flex justify-center  items-center transform underline transition-all ease-linear  duration-300 bg-black text-white  ${isClicked ? 'h-96 opacity-100' : 'opacity-0 h-0'}`}>
+           <div className="space-y-3">
+           {navLinks}
+           </div>
+        </div>
+        <p>hello</p>
     </nav>
   );
 };
