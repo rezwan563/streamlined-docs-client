03569cd905edfcd495d61b51bfa29cabb552dc88
Rezwan <t.rewan17@gmail.com>
2023-08-30 00:57:15 +0600

    Chatbox design

--- src/layout/DashboardLayout.jsx
+++ src/layout/DashboardLayout.jsx
@@ -1,11 +1,6 @@
-import { useContext, useState } from "react";
+import {  useState } from "react";
 import { Outlet } from "react-router-dom";
-import { AuthContext } from "../providers/AuthProvider";
 import Sidebar from "../shared/sidebar/Sidebar";
-import { IoMdNotificationsOutline } from "react-icons/io";
-import { FiSettings } from "react-icons/fi";
-import { FiMessageSquare } from "react-icons/fi";
-import { BsQuestionCircle } from "react-icons/bs";
 import { RiMenuFoldFill } from "react-icons/ri";
 import { RiMenuUnfoldFill } from "react-icons/ri";
 import DesktopNavbar from "../shared/desktopNavbar/DesktopNavbar";
@@ -20,7 +15,7 @@ const DashboardLayout = () => {
         <DesktopNavbar />
         <div className="hidden pt-8 lg:block">
           <div className="grid grid-cols-5 min-h-screen">
-            <div className="col-span-1">
+            <div className="col-span-1 ">
               <Sidebar />
             </div>
             <div className="col-span-4 bg-slate-100 px-8 py-12">
@@ -47,10 +42,10 @@ const DashboardLayout = () => {
             />
           </div>
           <div
-            className={`lg:hidden z-50 fixed top-24 text-lg bg-white rounded-s-md h-screen w-3/4  transition ease-linear duration-150 underline ${
+            className={`lg:hidden z-50 fixed top-24 text-lg bg-white border-l-2 border-slate-200 rounded-s-md h-screen w-3/4  transition ease-linear delay-200 underline ${
               isMenuClicked
-                ? "  translate-x-36 "
-                : " translate-x-[500px]"
+                ? "translate-x-24 md:translate-x-[500px]"
+                : "hidden"
             }`}
           >
             <Sidebar />
--- src/pages/dashboard/chat/Chatbox.jsx
+++ src/pages/dashboard/chat/Chatbox.jsx
@@ -1,20 +1,59 @@
-const Chatbox = () => {
+import { AiOutlineSend } from "react-icons/ai"
+const ChatBox = () => {
   return (
-    <div className="md:h-3/4 md:w-1/2 md:my-auto md:mx-auto bg-white rounded-md relative">
-      <div className="p-2">
-        <input className="border-2 border-slate-200 px-2" type="text" placeholder="username" />
-        <input className="border-2 border-slate-200 px-2" type="text" placeholder="room" />
-        <button className="p-2 bg-slate-400">Join Room</button>
+    <div className="w-full  bg-white rounded-lg px-1 md:px-2 shadow-md overflow-hidden">
+      <div className="p-4 border-b border-gray-300">
+        <h2 className="text-lg font-semibold">Chat with Support</h2>
       </div>
-      <div className="">
-        Body
+      <div className="p-4 h-56 overflow-y-auto">
+        <div className="flex flex-col space-y-2">
+          <div className="flex items-start space-x-2">
+            <img
+              src="https://i.ibb.co/R4wcyR4/360-F-214746128-31-Jkea-P6r-U0-Nzzzd-FC4kh-Gkmqc8noe6h.jpg" // Add your image source here
+              alt="User Avatar"
+              className="w-8 h-8 rounded-full"
+            />
+            <div>
+              <div className="bg-gray-200 rounded-lg p-2">
+                {/* sender message */}
+                <p>Hey, I need help with my account.</p>
+              </div>
+              <span>
+                {/* sender message time */}
+                <small>08: 32 pm</small>
+              </span>
+            </div>
+          </div>
+          <div className="flex items-start justify-end space-x-2">
+            <div>
+            <div className="bg-blue-600 text-white rounded-lg p-2">
+              {/* Self Message */}
+              <p>Sure, I'm here to assist you.</p>
+            </div>
+            {/* selft message time */}
+            <span><small>08: 33 pm</small></span>
+            </div>
+            <img
+              src="https://i.ibb.co/c6CMxHv/portrait-photography-neutral-background-1.jpg" // Add your image source here
+              alt="Support Avatar"
+              className="w-8 h-8 rounded-full"
+            />
+            
+          </div>
+        </div>
       </div>
-      <div className="absolute bottom-0 w-full">
-        <input className="px-2 py-1 w-3/4 border-2 border-slate-300 focus:outline-none text-md" type="text" placeholder="Type of Message" />
-        <button className="px-4 py-1 bg-blue-400">Send</button>
+      <div className="p-4 border-t flex items-center gap-2 border-gray-300">
+        <input
+          type="text"
+          placeholder="Type your message..."
+          className="w-4/5 border border-gray-300 rounded-md px-3 py-2 outline-none"
+        />
+        <button className=" bg-blue-600 text-white  rounded-md px-3 py-2">
+          <AiOutlineSend/>
+        </button>
       </div>
     </div>
   );
 };
 
-export default Chatbox;
+export default ChatBox;
