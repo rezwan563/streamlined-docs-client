a597fb51a79ad4fbfdd3928207bdc379d2be59be
Muhammad Rasel Bin Siddik <muhammadraselbinsiddik@gmail.com>
2023-08-31 13:26:41 +0600

    admin home page create

---
+++ src/pages/dashboard/dasboardsummary/AdminHomeChart.jsx
@@ -0,0 +1,80 @@
+
+import { BarChart, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Bar } from 'recharts';
+
+const AdminHomeChart = () => {
+    const data = [
+        {
+            name: 'Page A',
+            Pending: 4000,
+            Aproved: 240,
+            Reviwed: 2400,
+        },
+        {
+            name: 'Page B',
+            Pending: 2000,
+            Aproved: 3400,
+            Reviwed: 400,
+        },
+        {
+            name: 'Page C',
+            Pending: 400,
+            Aproved: 2000,
+            Reviwed: 2400,
+        },
+        {
+            name: 'Page D',
+            Pending: 400,
+            Aproved: 200,
+            Reviwed: 2400,
+        },
+        {
+            name: 'Page E',
+            Pending: 5500,
+            Aproved: 2400,
+            Reviwed: 240,
+        },
+        {
+            name: 'Page F',
+            Pending: 400,
+            Aproved: 400,
+            Reviwed: 200,
+        },
+        {
+            name: 'Page G',
+            Pending: 3000,
+            Aproved: 2400,
+            Reviwed: 2900,
+        },
+    ];
+    console.log(data);
+        return (
+            <div className='mt-10 bg-white'>
+                <h1 className='text-2xl font-bold mt-2 mb-2'>Admin Bar Chart</h1>
+
+                <div> <ResponsiveContainer width={500} height={300}>
+                    <BarChart
+                        width={500}
+                        height={300}
+                        data={data}
+                        margin={{
+                            top: 5,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="Pending" fill="blue" />
+                        <Bar dataKey="Aproved" fill="red" />
+                        <Bar dataKey="Reviwed" fill="orange" />
+                    </BarChart>
+                </ResponsiveContainer></div>
+            </div>
+        );
+    };
+
+export default AdminHomeChart
---
+++ src/pages/dashboard/dasboardsummary/AdminHomePieChart.jsx
@@ -0,0 +1,54 @@
+import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
+
+const data = [
+    { name: 'Group A', value: 400 },
+    { name: 'Group B', value: 300 },
+    { name: 'Group C', value: 300 },
+    { name: 'Group D', value: 200 },
+];
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+    return (
+        <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+            {`${(percent * 100).toFixed(0)}%`}
+        </text>
+    );
+};
+
+const AdminHomePieChart = () => {
+    return (
+        <div className='bg-white mt-10'>
+            <h1 className='text-2xl font-bold mt-2 mb-2'>Admin Pie Chart</h1>
+            <ResponsiveContainer width={300} height={300}>
+                <PieChart width={300} height={300}>
+                    <Pie
+                        data={data}
+                        cx="50%"
+                        cy="50%"
+                        labelLine={false}
+                        label={renderCustomizedLabel}
+                        outerRadius={80}
+                        fill="#8884d8"
+                        dataKey="value"
+                    >
+                        {data.map((entry, index) => (
+                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                        ))}
+                    </Pie>
+                    <Pie dataKey="Group A" fill="blue" />
+                    <Pie dataKey="Group B" fill="blue" />
+                    <Pie dataKey="Group C" fill="blue" />
+                    <Pie dataKey="Group D" fill="blue" />
+
+                </PieChart>
+            </ResponsiveContainer>
+        </div>
+    );
+};
+
+export default AdminHomePieChart;
