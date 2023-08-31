e4b56d9e09ef34526d0dfd145d9b24743d2a124a
Rezwan <t.rewan17@gmail.com>
2023-08-31 14:36:01 +0600

    Added google fonts in index.css

--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,21 +1,24 @@
 import Lottie from "lottie-react";
-import globe from "../../assets/globe_animation.json"
+import globe from "../../assets/globe_animation.json";
 import BannerBadge from "../../shared/bannerbadge/BannerBadge";
 const Homepage = () => {
-    
-    return (
-        <div className="lg:pt-24">
-            <div className="grid grid-cols-1 px-2 md:grid-cols-2">
-                <div className="">
-                    <BannerBadge/>
-                    <div className="lg:py-12">
-                        <p className="lg:text-7xl text-left font-bold font-sans">Streamlined Docs: Your Gateway to Uninterrupted Services</p>
-                    </div>
-                </div>
-                <div className=""><Lottie   animationData={globe}/></div>
-            </div>
+  return (
+    <div className="lg:pt-24">
+      <div className="grid grid-cols-1 px-2 md:grid-cols-2 w-full ">
+        <div className="">
+          <BannerBadge />
+          <div className="lg:py-12 w-full">
+            <p className="lg:text-7xl text-left font-semibold font-staatliches ">
+              Streamlined Docs: Your Gateway to Uninterrupted Services
+            </p>
+          </div>
         </div>
-    );
+        <div className="lg:w-[800px] hidden md:block ">
+          <Lottie animationData={globe} />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
--- src/shared/bannerbadge/BannerBadge.jsx
+++ src/shared/bannerbadge/BannerBadge.jsx
@@ -1,7 +1,7 @@
 const BannerBadge = () => {
   return (
     <div className="bg-red-200 px-2 rounded-full flex">
-      <div className="bg-red-500 text-white font-semibold px-1 rounded-xl mr-1">
+      <div className="bg-red-500 text-slate-100 font-semibold px-1 rounded-xl mr-1">
         <p>New</p>
       </div>
       <span className="text-red-800 font-medium">
