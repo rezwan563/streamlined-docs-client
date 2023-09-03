cb049d18e0ae48bdd8e8cda1e72bd9dd8dab5bea
Rezwan <t.rewan17@gmail.com>
2023-09-03 09:03:07 +0600

    Update npm

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "../shared/header/Header";
+import { useState } from "react";
 
 const MainLayout = () => {
+
   return (
     <div className="lg:max-w-7xl lg:mx-auto">
       <Header/>
-      <Outlet />
+      <Outlet/>
     </div>
   );
 };
