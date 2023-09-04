8c78c750108db90a65806c6df1cdb9f1941d7825
Rezwan <t.rewan17@gmail.com>
2023-09-05 04:34:49 +0600

    Made Login first and Sign up second in /auth

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -5,6 +5,7 @@ import { AuthContext } from '../../providers/AuthProvider';
 import Swal from 'sweetalert2'
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import 'react-tabs/style/react-tabs.css';
+import GoogleLogin from '../../shared/googlelogin/GoogleLogin';
 
 const Register = () => {
     const [show, setShow] = useState(true);
@@ -57,25 +58,13 @@ const Register = () => {
 
     }
 
-
-    const handleGoogleSignIn = () => {
-        signInWithGoogle()
-            .then(result => {
-                console.log(result.user)
-                navigate(from, { replace: true })
-            })
-            .catch(error => {
-                setError(error.message)
-            })
-    }
-
     return (
         <div className='flex justify-center items-center min-h-screen sm:py-12 bg-white'>
             <div className='flex flex-col max-w-lg p-6 rounded-md sm:p-5 w-full bg-gray-100 text-gray-900'>
                 <div className='mb-8 text-center'>
                     <h1 className='my-3 text-4xl font-bold'>Sign Up</h1>
                     <p className='text-sm text-gray-400'>
-                        Welcome to our passport app
+                        Welcome to our NID Correction app
                     </p>
                 </div>
                 <form
@@ -186,18 +175,11 @@ const Register = () => {
                 <div className='flex items-center pt-4 space-x-1'>
                     <div className='flex-1 h-px sm:w-16 dark:bg-gray-700'></div>
                     <p className='px-3 text-sm dark:text-gray-400'>
-                        Sign up with social accounts
+                        OR
                     </p>
                     <div className='flex-1 h-px sm:w-16 dark:bg-gray-700'></div>
                 </div>
-                <div
-                    onClick={handleGoogleSignIn}
-                    className='flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer'
-                >
-                    <FcGoogle size={32} />
-
-                    <p>Continue with Google</p>
-                </div>
+                <GoogleLogin/>
             </div>
         </div>
     );
