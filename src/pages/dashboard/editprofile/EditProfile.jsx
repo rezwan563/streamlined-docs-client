effed8414e9bdab7abe92baa0df52ca2b6b2e0d1
Rezwan <t.rewan17@gmail.com>
2023-09-04 19:28:10 +0600

    Created identification info component

--- src/pages/dashboard/editprofile/EditProfile.jsx
+++ src/pages/dashboard/editprofile/EditProfile.jsx
@@ -1,88 +1,23 @@
-import "./EditProfile.css"
+import IdentificationInfo from "../../../shared/identificationinfo/IdentificationInfo";
+import PersonalInformation from "../../../shared/personalinfo/PersonalInformation";
+import "./EditProfile.css";
 const EditProfile = () => {
   return (
     <div>
-      <div className="bg-white lg:max-w-5xl rounded-md">
-        <div className="p-4">
-          <div>
-            <p className="text-2xl font-bold">
-              Please fill up your profile information
-            </p>
-          </div>
-          <div className="py-2 mb-2 border-b-2 border-blue-700">
-            <p className=" font-bold text-base">
-              Personal Information
-            </p>
-          </div>
-          <div className="grid grid-cols-2 gap-4">
-            <div className="flex justify-between items-center">
-              <label htmlFor="name" className="font-normal text-base mr-2 ">
-                Name(bn)
-              </label>
-              <input
-                type="text"
-                name="name"
-                className="border-b-2 border-slate-200 pl-2 rounded-md py-1   text-xl  focus:border-blue-500 focus:outline-none transition duration-300 ease-in-out "
-                placeholder="নাম"
-              />
-            </div>
-            <div className="flex justify-between items-center">
-              <label htmlFor="name" className="font-normal text-base mr-2 ">
-                Name(en)
-              </label>
-              <input
-                type="text"
-                name="nameEnglish"
-                className="border-b-2 border-slate-200 pl-2 rounded-md py-1   text-xl  focus:border-blue-500 focus:outline-none transition duration-300 ease-in-out"
-                placeholder="Name"
-              />
-            </div>
-            <div className="flex justify-between items-center">
-              <label htmlFor="name" className="font-normal text-base mr-2 ">
-                Father Name(bn)
-              </label>
-              <input
-                type="text"
-                name="fatherName"
-                className="border-b-2 border-slate-200 pl-2 rounded-md py-1   text-xl  focus:border-blue-500 focus:outline-none transition duration-300 ease-in-out"
-                placeholder="পিতার নাম"
-              />
-            </div>
-            <div className="flex justify-between items-center">
-              <label htmlFor="name" className="font-normal text-base mr-2 ">
-                Father Name(en)
-              </label>
-              <input
-                type="text"
-                name="fatherNameEnglish"
-                className="border-b-2 border-slate-200 pl-2 rounded-md py-1   text-xl  focus:border-blue-500 focus:outline-none transition duration-300 ease-in-out"
-                placeholder="Father Name"
-              />
-            </div>
-            <div className="flex justify-between items-center">
-              <label htmlFor="name" className="font-normal text-base mr-2 ">
-                Mother Name(bn)
-              </label>
-              <input
-                type="text"
-                name="motherName"
-                className="border-b-2 border-slate-200 pl-2 rounded-md py-1   text-xl  focus:border-blue-500 focus:outline-none transition duration-300 ease-in-out"
-                placeholder="মাতার নাম"
-              />
-            </div>
-            <div className="flex justify-between items-center">
-              <label htmlFor="name" className="font-normal text-base mr-2 ">
-                Mother Name(bn)
-              </label>
-              <input
-                type="text"
-                name="motherNameEnglish"
-                className="border-b-2 border-slate-200 pl-2 rounded-md py-1   text-xl  focus:border-blue-500 focus:outline-none transition duration-300 ease-in-out"
-                placeholder="Mother Name"
-              />
-            </div>
-          </div>
+      <div
+        className="bg-white 
+      lg:max-w-5xl rounded-md p-4"
+      >
+        <div>
+          <p className="text-2xl font-bold">
+            Please fill up your profile information
+          </p>
         </div>
+        <PersonalInformation />
+        <div className="py-2 my-8 border-b-2 border-blue-700">
+          <p className=" font-bold text-base">Identification Information</p>
+        </div>
+        <IdentificationInfo />
       </div>
     </div>
   );
---
+++ src/shared/identificationinfo/IdentificationInfo.jsx
@@ -0,0 +1,98 @@
+const IdentificationInfo = () => {
+  return (
+    <div className="">
+      <div className="grid grid-cols-2 gap-4">
+        <div className="flex justify-between items-center">
+          <label htmlFor="name" className="font-normal text-base mr-2 ">
+            Education(bn)
+          </label>
+          <input
+            type="text"
+            name="education"
+            className="border-b-2 border-slate-200 pl-2 rounded-md py-1   text-xl  focus:border-blue-500 focus:outline-none transition duration-300 ease-in-out "
+            placeholder="শিক্ষাগত যোগ্যতা"
+          />
+        </div>
+        <div className="flex justify-between items-center">
+          <label htmlFor="name" className="font-normal text-base mr-2 ">
+            Occupation
+          </label>
+          <input
+            type="text"
+            name="occupation"
+            className="border-b-2 border-slate-200 pl-2 rounded-md py-1   text-xl  focus:border-blue-500 focus:outline-none transition duration-300 ease-in-out"
+            placeholder="পেশা"
+          />
+        </div>
+        <div className="flex justify-between items-center">
+          <label htmlFor="name" className="font-normal text-base mr-2 ">
+            Reliogion(bn)
+          </label>
+          <input
+            type="text"
+            name="religion"
+            className="border-b-2 border-slate-200 pl-2 rounded-md py-1   text-xl  focus:border-blue-500 focus:outline-none transition duration-300 ease-in-out"
+            placeholder="ধর্ম"
+          />
+        </div>
+        <div className="flex justify-between items-center">
+          <label htmlFor="name" className="font-normal text-base mr-2 ">
+            Phone
+          </label>
+          <input
+            type="text"
+            name="phone"
+            className="border-b-2 border-slate-200 pl-2 rounded-md py-1   text-xl  focus:border-blue-500 focus:outline-none transition duration-300 ease-in-out"
+            placeholder="Phone"
+          />
+        </div>
+        <div className="flex justify-between items-center">
+          <label htmlFor="name" className="font-normal text-base mr-2 ">
+            Driving License Number
+          </label>
+          <input
+            type="text"
+            name="drivingLicense"
+            className="border-b-2 border-slate-200 pl-2 rounded-md py-1   text-xl  focus:border-blue-500 focus:outline-none transition duration-300 ease-in-out"
+            placeholder="Driving License"
+          />
+        </div>
+        <div className="flex justify-between items-center">
+          <label htmlFor="name" className="font-normal text-base mr-2">
+            Passport Number
+          </label>
+          <input
+            type="text"
+            name="passportNumber"
+            className="border-b-2 border-slate-200 pl-2 rounded-md py-1   text-xl  focus:border-blue-500 focus:outline-none transition duration-300 ease-in-out"
+            placeholder="Passport Number"
+          />
+        </div>
+        <div className="flex justify-between items-center">
+          <label htmlFor="name" className="font-normal text-base mr-2">
+            TIN Number
+          </label>
+          <input
+            type="text"
+            name="tinNumber"
+            className="border-b-2 border-slate-200 pl-2 rounded-md py-1   text-xl  focus:border-blue-500 focus:outline-none transition duration-300 ease-in-out"
+            placeholder="TIN Number"
+          />
+        </div>
+        <div className="flex justify-between items-center">
+          <label htmlFor="name" className="font-normal text-base mr-2">
+            Identification Mark(bn)
+          </label>
+          <input
+            type="text"
+            name="identificationMark"
+            className="border-b-2 border-slate-200 pl-2 rounded-md py-1   text-xl  focus:border-blue-500 focus:outline-none transition duration-300 ease-in-out"
+            placeholder="Identification Mark"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default IdentificationInfo;
