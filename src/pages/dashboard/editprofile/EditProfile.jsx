ea998873c34997f319543381172051fc6607b3d6
Rezwan <t.rewan17@gmail.com>
2023-09-05 16:00:22 +0600

    Completed EditProfile component design

--- src/pages/dashboard/editprofile/EditProfile.jsx
+++ src/pages/dashboard/editprofile/EditProfile.jsx
@@ -1,12 +1,12 @@
+import AddressInfo from "../../../shared/addressinfo/AddressInfo";
 import IdentificationInfo from "../../../shared/identificationinfo/IdentificationInfo";
 import PersonalInformation from "../../../shared/personalinfo/PersonalInformation";
-import "./EditProfile.css";
 const EditProfile = () => {
   return (
     <div>
       <div
-        className="bg-white 
-      lg:max-w-5xl rounded-md p-4"
+        className="bg-white w-full md:max-w-5xl 
+      md:mx-auto rounded-md md:p-12 px-4"
       >
         <div>
           <p className="text-2xl font-bold">
@@ -18,6 +18,16 @@ const EditProfile = () => {
           <p className=" font-bold text-base">Identification Information</p>
         </div>
         <IdentificationInfo />
+        <div className="py-2 my-8 border-b-2 border-blue-700">
+          <p className="font-bold text-base">Address Information</p>
+        </div>
+        <AddressInfo />
+        <div className="flex items-center justify-between pt-4">
+          <input type="file" name="profile-picture" id="" />
+          <button className="bg-blue-700 px-4 py-2 mt-4 text-end rounded-md text-white font-semibold">
+            Submit
+          </button>
+        </div>
       </div>
     </div>
   );
--- src/shared/header/Header.jsx
+++ src/shared/header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
   const navLinks = (
     <>
       <ActiveLink to="/">HOME</ActiveLink>
-      <ActiveLink to="/">DETAILS</ActiveLink>
+      <ActiveLink to="/details">DETAILS</ActiveLink>
       <ActiveLink to="/faq">FAQ</ActiveLink>
       <ActiveLink to="/dashboard">DASHBOARD</ActiveLink>
     </>
