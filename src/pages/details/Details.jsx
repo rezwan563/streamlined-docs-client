32f4fc48587b7f2ef761fe6ae76855b7fb15fbb0
Muhammad Rasel Bin Siddik <muhammadraselbinsiddik@gmail.com>
2023-09-07 13:01:29 +0600

    button create in details page

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,18 +1,19 @@
 import demo4 from '../../assets/Images/demo4.png'
+
 const Details = () => {
     return (
         <div className="pt-12 md:pt-24 ">
             <div className="grid grid-cols-1 px-2 lg:grid-cols-2 gap-8 max-w-screen-xl">
-                <div className="space-y-8 md:space-y-12">
+                <div className="space-y-8 md:space-y-12 text-xl">
                    ABOUT US
                     <div className="">
                         <p className="text-5xl lg:text-5xl text-left font-semibold font-staatliches ">
-                            Streamlined Docs Your Gateway to Uninterrupted Services.
+                            StreamlinedDocs - Your Personal Document Correction Partner.
                         </p>
-                        <p className="mt-3 mb-3">Lorem ipsum dolor, sit amet consectetur adipisicing elit. A molestiae sint sunt delectus adipisci aliquid distinctio obcaecati nisi nemo dolore exercitationem magnam quia quas, perspiciatis ullam odit eos neque. Nam.</p>
+                        <p className="mt-3 mb-3">In todays digital age, the accuracy and reliability of national documents like National ID Cards (NID) and Passports are paramount. Recognizing the need for a seamless and user-centric solution,  With a focus on efficiency and convenience, StreamlinedDocs empowers users to review, update, and rectify discrepancies within their official records, revolutionizing the traditional document correction process</p>
                     </div>
                     <div className="hidden lg:block">
-                        <button className="bg-red-700 px-8 py-3 lg:px-12 lg:py-5 text-white text-lg font-semibold mr-5 lg:mr-12 hover:bg-red-600 hover:text-neutral-50">
+                        <button className="btn bg-red-600 text-lg font-semibold text-white  mb-3">
                             Sign Up For Free
                         </button>
                     </div>
@@ -21,15 +22,10 @@ const Details = () => {
                     <img src={demo4} alt="" />
                 </div>
                 <div className="block space-y-5 lg:hidden w-full">
-                    <button className="bg-red-700 px-8 py-3 w-full lg:px-12 lg:py-5 text-white text-lg font-semibold mr-5 lg:mr-12 hover:bg-red-600 hover:text-neutral-50">
+                    <button className="btn bg-red-600 text-lg font-semibold text-white w-full  mb-3">
                         Sign Up For Free
                     </button>
-                
                 </div>
-            </div>
-            <div className="text-5xl lg:text-5xl text-left font-semibold font-staatliches bg-slate-100">
-                Streamlined Docs Your Gateway to Uninterrupted Services.
-
             </div>
         </div>
     );
